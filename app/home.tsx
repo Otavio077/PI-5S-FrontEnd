e33094f5bfa1cb96ce4f6624c8244a84d007bd9c
Otavio077 <88412710+Otavio077@users.noreply.github.com>
2024-03-03 11:05:10 -0300

    Criação do repositório

---
+++ app/home.tsx
@@ -0,0 +1,11 @@
+import { Text } from 'react-native'
+import { Link } from 'expo-router'
+
+export default function Home(){
+    return(
+        <>
+            <Text>Login com sucesso</Text>
+            <Link href="/">Voltar ao login</Link>
+        </>
+    )
+}
